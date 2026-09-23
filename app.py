fbeed25ee0da0bdc9bfae6f11420319e4d373a65
wagneralexander <alexander.wagner@liv.systems>
2026-09-23 11:07:20 +0200

    Implement main page

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
+DATA_FILE = "data/blog_db.json"
+
+def load_posts():
+    with open(DATA_FILE, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+def save_posts(posts):
+    with open(DATA_FILE, "w", encoding="utf-8") as file:
+        json.dump(posts, file, indent=2, ensure_ascii=False)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
