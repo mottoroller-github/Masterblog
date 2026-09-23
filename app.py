26d7525212184594cd5d85e58e085dbcb3d10908
wagneralexander <alexander.wagner@liv.systems>
2026-09-23 12:01:41 +0200

    Add delete route for blog posts

--- app.py
+++ app.py
@@ -40,6 +40,19 @@ def add():
 
     return render_template('add.html')
 
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    blog_posts = load_posts()
+
+    blog_posts = [
+        post for post in blog_posts
+        if post["id"] != post_id
+    ]
+
+    save_posts(blog_posts)
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
